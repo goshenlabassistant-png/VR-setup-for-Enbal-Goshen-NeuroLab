36550666dd1dd8e24d6c1de04b562ef29331ef3c
eldar <eldar.hacohen@mail.huji.ac.il>
2025-12-17 13:00:01 +0200

    .

--- expDesktopApp/src/main/java/App.java
+++ expDesktopApp/src/main/java/App.java
@@ -7,10 +7,12 @@ import mainFlow.ExperimentFlow;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ExperimentData expData = new ExperimentData("A1", "B1", "");
+        ExperimentData expData = new ExperimentData("GCamp_Gq19", "M1_trainig", "C:\\Users\\owner\\Desktop\\Shay");
         ExperimentFlow exp = new ExperimentFlow(35, expData);
         ArrayList<RewardStationDef> rewards = new ArrayList<>();
-        rewards.add(new RewardStationDef(3, 2, 1, 0));
-        exp.startExp(new File("C:\\Users\\Yonina\\OneDrive - Jerusalem College of Technology - Machon Lev\\Documents\\neuro_lab\\Blender mazes\\training\\training_csv_random_rew_new_small_treadmill.blend.exe"), rewards);
+        rewards.add(new RewardStationDef(5, 1, 1, 0));
+        rewards.add(new RewardStationDef(5, 1, 1, 0));
+        rewards.add(new RewardStationDef(5, 1, 1, 0));
+        exp.startExp(new File("C:\\Users\\owner\\Desktop\\new_bhv\\main_maze\\env _A_csv_random_rew_new_small_treadmill.blend.exe"), rewards);
     }
 }
