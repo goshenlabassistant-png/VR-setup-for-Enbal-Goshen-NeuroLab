01e4a278afb78a6a8a9afad45feb3fdd3da994b6
eldar <eldar.hacohen@mail.huji.ac.il>
2025-12-24 09:11:20 +0200

    .

--- expDesktopApp/src/main/java/App.java
+++ expDesktopApp/src/main/java/App.java
@@ -8,15 +8,15 @@ import mainFlow.ExperimentFlow;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ExperimentData expData = new ExperimentData("GCamp_Gq20", "M1_trainig", "C:\\Users\\owner\\Desktop\\Shay");
+        ExperimentData expData = new ExperimentData("GCamp_Gq19 ", "M1_training", "C:\\Users\\owner\\Desktop\\Shay");
         ExperimentFlow exp = new ExperimentFlow(35, expData);
         ArrayList<RewardStationDef> rewards = new ArrayList<>();
 
-        rewards.add(new RewardStationDef(3, 2));
-        // rewards.add(new RewardStationDef(4, 2, 
-        //     new int[] {1,2,4,5}, // zones
-        //     new int[] {0,0,1,-1}, // placeInZone
-        //     new double[] {1.0,0.9,0.7,1.0})); // probability
+        // rewards.add(new RewardStationDef(1, 2));
+        rewards.add(new RewardStationDef(5, 2, 
+            new int[] {1,2,3,4,5}, // zones
+            new int[] {0,0,0,0,0}, // placeInZone
+            new double[] {1.0,1.0,1.0,1.0,1.0})); // probability
 
         exp.startExp(new File("C:\\Users\\owner\\Desktop\\new_bhv\\main_maze\\env _A_csv_random_rew_new_small_treadmill.blend.exe"), rewards);
     }
